6564bb931ba80276be7ea47ef8d51ad85cd118aa
Rahmann Fahim <rahmannfahim001@gmail.com>
2026-06-17 00:15:46 +0600

    Add files via upload

---
+++ Lab2/task4.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character: ")
+
+if ch.lower() in "aeiou":
+    print("Vowel")
+else:
+    print("Not Vowel")
---
+++ Lab2/task5.py
@@ -0,0 +1,22 @@
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade: A+")
+elif marks >= 85:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B+")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C+")
+elif marks >= 65:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D+")
+elif marks >= 55:
+    print("Grade: D")
+elif marks >= 50:
+    print("Grade: D")
+else:
+    print("Grade: F")
